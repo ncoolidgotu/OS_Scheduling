79e26c44499deab53263ef1c75620455755ec39e
ncoolidgotu <nathanael.coolidge@ontariotechu.net>
2023-03-19 18:23:53 -0400

    modified:   .DS_Store 	renamed:    main/MainWindow.ui -> MainWindow.ui 	renamed:    main/background.qrc -> background.qrc 	renamed:    main/bgS.png -> bgS.png 	new file:   bgm.mp3 	modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 from PyQt6 import QtWidgets #For Changing GUI Screens
 from PyQt6.QtCore import QDate, QDateTime
 from PyQt6.QtWidgets import QApplication, QMainWindow, QErrorMessage, QMessageBox #For GUI Functionality
@@ -12,11 +13,27 @@ class MainWindow(QMainWindow): #Derived class of QMainWindow to control function
         super(MainWindow,self).__init__()
         loadUi("MainWindow.ui",self)  # Loads the window from main menu .ui file
         #self.inputFile.clicked.connect(ProcessReader.selectFile)  
-        self.showGraph.clicked.connect(Graph.show)              
+        self.showGraph.clicked.connect(Graph.show)
+        graph = Graph(self)
+        vbox.addWidget(graph)
         
+        
+class GUI: #Class to control GUI windows
+    def __init__(self):
+        self.app = QtWidgets.QApplication(sys.argv) #Widgets is used to organize GUI windows
+        self.screen =  QtWidgets.QStackedWidget() #Stacked widgets allows multiple windows to be created
+        self.mainMenu = MainWindow() #Set first window to Main Menu UI
+        
+    def buildGUI(self): #Build the GUI
+            self.screen.addWidget(self.mainMenu) #Screen Index 0
+            self.screen.setFixedHeight(720) #constrain window dimensions
+            self.screen.setFixedWidth(1280)
+            self.screen.show() #Display window
+            self.app.exec() #Execute GUI
+
 class Graph(FigureCanvas):
     def __init__(self, parent):
-        fig, self.ax = plt.subplots(figsize=(5, 4), dpi=200)
+        fig, self.ax = plt.subplots(figsize=(4, 3), dpi=200)
         super().__init__(fig)
         self.setParent(parent)
 
@@ -31,3 +48,11 @@ class Graph(FigureCanvas):
         self.ax.set(xlabel='time (s)', ylabel='voltage (mV)',
             title='About as simple as it gets, folks')
         self.ax.grid()
+        
+if __name__ == "__main__":
+    pygame.mixer.init()     # loads the background music
+    pygame.mixer.music.load("bgm.mp3")
+    pygame.mixer.music.play()
+    gui = GUI()
+    gui.buildGUI()
+
